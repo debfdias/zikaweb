1708bf16048b550aff47204e9d6e35f44be63c21
debfdias <debfdias@gmail.com>
2018-08-07 14:50:54 -0300

    bd heroku

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
 	database: {
 		multipleStatements: true,
-		host:	  'localhost', 	// database host
-		user: 	  'root', 		// your database username
-		password: '', 		// your database password
+		host:	  'us-cdbr-iron-east-01.cleardb.net', 	// database host
+		user: 	  'b87ad073f0d63d', 		// your database username
+		password: '2d8c7b6b', 		// your database password
 		port: 	  3306, 		// default MySQL port
-		db: 	  'sampledb' 		// your database name
+		db: 	  'heroku_77659378f7bfef1' 		// your database name
 	},
 	server: {
 		host: '127.0.0.1',
